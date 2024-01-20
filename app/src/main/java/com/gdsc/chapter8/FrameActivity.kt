d828f73b14c8df2ab3daa1913fc997e10b1026b8
Yu-jimin <jiminyu0114@gmail.com>
2024-01-20 20:49:30 +0900

    feat: 나만의 갤러리 앱 구현

---
+++ app/src/main/java/com/gdsc/chapter8/FrameActivity.kt
@@ -0,0 +1,48 @@
+package com.gdsc.chapter8
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.gdsc.chapter8.databinding.ActivityFrameBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class FrameActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFrameBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFrameBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_frame)
+
+        binding.toolBar.apply {
+            title = "나만의 앨범"
+            setSupportActionBar(this)
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val images = (intent.getStringArrayExtra("images") ?: emptyArray())
+            .map { uriString -> FrameItem(Uri.parse(uriString)) }
+        val frameAdapter = FrameAdapter(images)
+
+        binding.viewPager.adapter = frameAdapter
+
+        TabLayoutMediator(
+            binding.tabLayout,
+            binding.viewPager,
+        ) {
+            tab, position ->
+            binding.viewPager.currentItem = tab.position
+        }.attach()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
